cd4477a1b797471ba70b4b964e5c4d86eeae2feb
Yuri Abibok <yuri.abibok@intergiro.tech>
2020-05-30 17:08:52 +0300

    Revert sequelize

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getHello(): Promise<void> {
-    await this.appService.getHello();
+  getHello() {
+    return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,30 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Sequelize } from 'sequelize-typescript';
-import { User } from '../models/user';
 
 @Injectable()
 export class AppService {
-  async getHello(): Promise<void> {
-
-    const sequelize = new Sequelize({
-      database: 'imin',
-      dialect: 'mysql',
-      username: 'root',
-      password: 'M0tivati0n',
-      host: 'localhost',
-      port: 3300,
-    });
-
-    sequelize.addModels([User])
-
-    // sequelize.sync().then(result => {
-    //   console.log(result);
-    // })
-    //   .catch(err => console.log(err));
-
-
-    // const playerRepository = sequelize.getRepository(User);
-    // const player = await playerRepository.findOne({ where: { name: 'Yuri' } });
-    // return player;
+  getHello(): any {
+    return "Hello, World!"
   }
 }
