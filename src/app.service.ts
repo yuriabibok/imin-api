1555884076dc374ce1893135852917b37f1d3f21
Yuri Abibok <yuri.abibok@intergiro.tech>
2020-05-30 15:21:05 +0300

    Add docker

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Sequelize } from 'sequelize-typescript';
+import { User } from '../models/user';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<void> {
+
+    const sequelize = new Sequelize({
+      database: 'imin',
+      dialect: 'mysql',
+      username: 'root',
+      password: 'M0tivati0n',
+      host: 'localhost',
+      port: 3300,
+    });
+
+    sequelize.addModels([User])
+
+    // sequelize.sync().then(result => {
+    //   console.log(result);
+    // })
+    //   .catch(err => console.log(err));
+
+
+    // const playerRepository = sequelize.getRepository(User);
+    // const player = await playerRepository.findOne({ where: { name: 'Yuri' } });
+    // return player;
   }
 }
